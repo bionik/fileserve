723dc901b7f766bf5abd93075fd2fb6ec044fbbf
Lauri Hakkarainen <late@Rausku.local>
2017-11-12 17:07:30 +0100

    Testing file listing

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var formidable = require('formidable'),
-    http = require('http'),
-    util = require('util');
+/*jshint esversion: 6 */
+
+const formidable = require('formidable');
+const http = require('http');
+const fs = require('fs');
+
+const upload_folder = 'uploads';
 
 http.createServer(function(req, res) {
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    // parse a file upload
 
     var form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
+      res.writeHead(200, {'content-type': 'application/json'});
+
+      fs.readdir(upload_folder, (err, files) => {
+        var response = {'status': 'OK', 'files': files};
+        res.end(JSON.stringify(response));
+      });
+
     });
 
     return;
@@ -21,8 +28,7 @@ http.createServer(function(req, res) {
   res.writeHead(200, {'content-type': 'text/html'});
   res.end(
     '<form action="/upload" enctype="multipart/form-data" method="post">'+
-    '<input type="text" name="title"><br>'+
-    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="file" name="upload"><br>'+
     '<input type="submit" value="Upload">'+
     '</form>'
   );
