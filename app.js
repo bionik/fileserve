ec6f0fcd7130b69a4f7e4699f00730dc8478e649
Lauri Hakkarainen <late@Rausku.local>
2017-11-12 19:30:19 +0100

    Upload works, separated form html to index.html

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
 /*jshint esversion: 6 */
-
 const formidable = require('formidable');
 const http = require('http');
 const fs = require('fs');
+const util = require('util');
 
 const upload_folder = 'uploads';
 
-http.createServer(function(req, res) {
-  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+try {
+  fs.lstatSync(upload_folder).isDirectory();
+} catch(e){
+   if(e.code == 'ENOENT'){
+    console.log('ERROR: Upload directory does not exist!');
+    return false;
+   }
+}
 
+http.createServer(function(req, res) {
+  //Check if there is a POST to /upload
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    console.log('Processing POST to /upload');
     var form = new formidable.IncomingForm();
 
-    form.parse(req, function(err, fields, files) {
+    form.parse(req, function(err, fields, files){
+      var file = files.upload;
+
       res.writeHead(200, {'content-type': 'application/json'});
 
-      fs.readdir(upload_folder, (err, files) => {
-        var response = {'status': 'OK', 'files': files};
-        res.end(JSON.stringify(response));
-      });
+      console.log('Trying to move '+file.path+' to '+upload_folder+'/'+file.name);
 
+      fs.rename(file.path, upload_folder+'/'+file.name, function(err){
+        if(err){
+          console.log('ERROR: Move failed');
+          res.end(JSON.stringify({'status': 'FAIL'}));
+          return;
+        }
+
+        console.log('File move succeeded');
+
+        fs.readdir(upload_folder, function(err, files){
+          res.end(JSON.stringify({'status': 'OK', 'files': files}));
+        });
+
+      });
     });
 
     return;
   }
 
-  // show a file upload form
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.end(
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+
-    '<input type="file" name="upload"><br>'+
-    '<input type="submit" value="Upload">'+
-    '</form>'
-  );
+  //Show the default page
+  fs.readFile('index.html', 'utf8', function (err, data) {
+    if(err){
+      console.log('ERROR: index.html cannot be read!');
+      res.writeHead(404, {'content-type': 'text/html'});
+      res.end('404');
+      return false;
+    }
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(data);
+  });
 
 }).listen(8080);
